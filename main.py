cb6ab2342c759c39bc7d39e4f9dd7e5a6189c516
Henry Apler <henry.apler@gmail.com>
2023-09-26 14:18:41 +0200

    "made changes"

--- main.py
+++ main.py
@@ -5,22 +5,36 @@ from rich.table import Table
 from rich.progress import track
 import rich.prompt
 
-q = pd.read_csv('questions.csv')
+data = pd.read_csv('questions.csv', delimiter='.')
+q = data['Questions']
+answers = []
+range_str = list(range(1,8))
+# print(q[3])
 
-print(q)
+for i in range(len(q)):
+    answer = rich.prompt.IntPrompt.ask(q[i], choices=str(range_str), show_choices=True)  
+    answers.append(answer)
+a = pd.read_csv('answers.csv', delimiter=',')
+
+a.loc[len(a)] = answers
+a.to_csv('answers.csv',index=False)
+
+ext_int = a.loc[:,'q1':'q4'].sum().mean() 
+fee_thi = a.loc[:,'q5':'q8'].sum().mean()
+per_jud = a.loc[:,'q9':'q12'].sum().mean()
+sen_int = a.loc[:,'q13':'q16'].sum().mean()
+
+new_row1 = pd.DataFrame({'Extroversion - Introversion': ext_int},index=True)
+new_row2 = {'Feeling - Thinking': fee_thi}
+new_row3 = {'Perceiving - Judging': per_jud}
+new_row4 = {'Sensing - Intuition': sen_int}
+
+
+mean_a = pd.read_csv('mean_answers.csv',delimiter=',')
+mean_adf = pd.concat([mean_a,new_row1],axis=0,ignore_index=True)
+print(mean_adf)
 
 
 
-# range_str = range(1,7)
-# while True:
-#     answer = rich.prompt.IntPrompt.ask('Guess a number between 1 and 100: ', choices=range_str, show_choices=False)
-#     if answer == secret_number:
-#         print('Congratulations, you guessed the secret number!')
-#         break
-#     elif answer > secret_number:
-#         print('A little high')
-#     else:
-#         print('a little low')
-# play_again = rich.prompt.Confirm.ask('Play again?')
 
-#spara mean i kategorier 
+# spara mean i kategorier 
