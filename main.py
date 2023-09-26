470523d6664f64118dce03373c0d849de51588f2
dancingalfred <robban021@hotmail.com>
2023-09-26 14:34:06 +0200

    fixed first problem

--- main.py
+++ main.py
@@ -19,20 +19,28 @@ a = pd.read_csv('answers.csv', delimiter=',')
 a.loc[len(a)] = answers
 a.to_csv('answers.csv',index=False)
 
-ext_int = a.loc[:,'q1':'q4'].sum().mean() 
-fee_thi = a.loc[:,'q5':'q8'].sum().mean()
-per_jud = a.loc[:,'q9':'q12'].sum().mean()
-sen_int = a.loc[:,'q13':'q16'].sum().mean()
+ext_int = ['Extroversion - Introversion',a.loc[:,'q1':'q4'].sum().mean()]
+fee_thi = ['Feeling - Thinking',a.loc[:,'q5':'q8'].sum().mean()]
+per_jud = ['Perceiving - Judging',a.loc[:,'q9':'q12'].sum().mean()]
+sen_int = ['Sensing - Intuition',a.loc[:,'q13':'q16'].sum().mean()]
 
-new_row1 = pd.DataFrame({'Extroversion - Introversion': ext_int},index=True)
-new_row2 = {'Feeling - Thinking': fee_thi}
-new_row3 = {'Perceiving - Judging': per_jud}
-new_row4 = {'Sensing - Intuition': sen_int}
+# new_row1 = pd.DataFrame({'Extroversion - Introversion': ext_int},index=True)
+# new_row2 = {'Feeling - Thinking': fee_thi}
+# new_row3 = {'Perceiving - Judging': per_jud}
+# new_row4 = {'Sensing - Intuition': sen_int}
 
 
 mean_a = pd.read_csv('mean_answers.csv',delimiter=',')
-mean_adf = pd.concat([mean_a,new_row1],axis=0,ignore_index=True)
-print(mean_adf)
+
+mean_a.loc[len(a)] = ext_int
+mean_a.loc[len(a)] = fee_thi
+mean_a.loc[len(a)] = per_jud
+mean_a.loc[len(a)] = sen_int
+mean_a.to_csv('mean_answers.csv',index=False)
+
+
+#mean_adf = pd.concat([mean_a,new_row1],axis=0,ignore_index=True)
+#print(mean_a)
 
 
 
